0664c186c8072260aaba5c27f0f9669b4012fa3f
Marcelo Hernandez Lopez <marcelohdez.inq@gmail.com>
2024-02-18 17:44:07 -0500

    new: only exit successfully after 30 seconds

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use anyhow::{Context, Ok};
+use std::{process, thread, time::Duration};
+
+use anyhow::{anyhow, Context};
 use dim::dim::DimData;
 use smithay_client_toolkit::{
     compositor::CompositorState,
@@ -19,15 +21,16 @@ fn main() -> anyhow::Result<()> {
     let layer_shell = LayerShell::bind(&globals, &qh).context("Layer shell failed?")?;
     let mut data = DimData::new(compositor, &globals, &qh, layer_shell);
 
-    loop {
+    thread::spawn(|| {
+        thread::sleep(Duration::from_secs(30));
+        process::exit(0);
+    });
+
+    while !data.should_exit() {
         event_queue
             .blocking_dispatch(&mut data)
             .context("Failed to block on events!")?;
-
-        if data.should_exit() {
-            break;
-        }
     }
 
-    Ok(())
+    Err(anyhow!("Some user input was detected!"))
 }
