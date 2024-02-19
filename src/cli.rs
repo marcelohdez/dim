8f720362314afc8f1ff3803469d5c643308a164b
Marcelo Hernandez <marcelohdez.inq@gmail.com>
2024-02-19 01:45:55 -0500

    src: use actual default variables in cli help
    This makes it easier to maintain in case of changes

--- src/cli.rs
+++ src/cli.rs
@@ -1,19 +1,21 @@
 use clap::Parser;
 
+use crate::{DEFAULT_ALPHA, DEFAULT_DURATION};
+
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
 pub struct DimOpts {
     #[arg(
         short,
         long,
-        help = "Duration in seconds, 0 is infinite, default is 30"
+        help = format!("Duration in seconds, 0 is infinite, default is {DEFAULT_DURATION}")
     )]
     pub duration: Option<u64>,
 
     #[arg(
         short,
         long,
-        help = "0.0 is transparent, 1.0 is opaque, default is 0.5"
+        help = format!("0.0 is transparent, 1.0 is opaque, default is {DEFAULT_ALPHA}")
     )]
     pub alpha: Option<f32>,
 }
