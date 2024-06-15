bd5a0fa75274461d45b19ea2d6771d41b8fed7af
Marcelo <marcelohdez.inq@gmail.com>
2024-06-15 00:38:18 -0400

    src: cleanup main

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use directories::ProjectDirs;
 use log::{debug, info};
 use smithay_client_toolkit::{
     compositor::CompositorState,
-    reexports::client::{globals::registry_queue_init, Connection},
+    reexports::client::{globals::registry_queue_init, Connection, EventQueue},
     shell::wlr_layer::LayerShell,
 };
 
@@ -20,32 +20,10 @@ fn main() -> anyhow::Result<()> {
         return Ok(());
     }
 
-    let Some(dirs) = ProjectDirs::from("com", "marcelohdez", "dim") else {
-        bail!("Could not generate project directories on this OS?");
+    let opts = match get_config()? {
+        Some(config) => config.merge_onto_self(args),
+        None => args,
     };
-    let config_dir = dirs.config_dir().join(CONFIG_FILENAME);
-
-    let opts = if config_dir.exists() {
-        debug!("Config file found at {config_dir:?}");
-
-        let file = File::open(config_dir)?;
-        let config: DimOpts = toml::from_str(&read_to_string(file)?)?;
-
-        debug!("Config: {config:?}");
-        config.merge_onto_self(args)
-    } else {
-        info!("Config file not found!");
-        args
-    };
-
-    let conn = Connection::connect_to_env().context("Failed to connect to environment")?;
-
-    let (globals, mut event_queue) =
-        registry_queue_init(&conn).context("Failed to initialize registry")?;
-    let qh = event_queue.handle();
-
-    let compositor = CompositorState::bind(&globals, &qh).context("Compositor not available")?;
-    let layer_shell = LayerShell::bind(&globals, &qh).context("Layer shell failed?")?;
 
     debug!("Using options: {opts:?}");
     let alpha = opts.alpha.unwrap_or(DEFAULT_ALPHA);
@@ -59,7 +37,7 @@ fn main() -> anyhow::Result<()> {
         });
     }
 
-    let mut data = DimData::new(compositor, &globals, &qh, layer_shell, alpha);
+    let (mut data, mut event_queue) = create_wl_app(alpha)?;
     while !data.should_exit() {
         event_queue
             .blocking_dispatch(&mut data)
@@ -67,4 +45,40 @@ fn main() -> anyhow::Result<()> {
     }
 
     Err(anyhow!("Some user input was detected!"))
+}
+
+fn get_config() -> anyhow::Result<Option<DimOpts>> {
+    let Some(dirs) = ProjectDirs::from("com", "marcelohdez", "dim") else {
+        bail!("Could not generate project directories on this OS?");
+    };
+    let config_dir = dirs.config_dir().join(CONFIG_FILENAME);
+
+    if !config_dir.exists() {
+        info!("No config found!");
+        return Ok(None);
+    }
+
+    debug!("Config file found at {config_dir:?}");
+
+    let file = File::open(config_dir)?;
+    let config: DimOpts = toml::from_str(&read_to_string(file)?)?;
+
+    debug!("Config: {config:?}");
+    Ok(Some(config))
+}
+
+fn create_wl_app(alpha: f32) -> anyhow::Result<(DimData, EventQueue<DimData>)> {
+    let conn = Connection::connect_to_env().context("Failed to connect to environment")?;
+
+    let (globals, event_queue) =
+        registry_queue_init(&conn).context("Failed to initialize registry")?;
+    let qh = event_queue.handle();
+
+    let compositor = CompositorState::bind(&globals, &qh).context("Compositor not available")?;
+    let layer_shell = LayerShell::bind(&globals, &qh).context("Layer shell failed?")?;
+
+    Ok((
+        DimData::new(compositor, &globals, &qh, layer_shell, alpha),
+        event_queue,
+    ))
 }
